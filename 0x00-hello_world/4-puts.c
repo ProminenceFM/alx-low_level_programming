f011a861d16cefb2b3d6e8de3bff4390920925d0
Folarin <folarinaluko123@yahoo.com>
2022-10-14 08:32:36 -0700

    kdkdk2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - printing using puts funtion
  *
- * Return: Always 0 (Success)
- * /
- 
+ * Return: always 0
+*/
  int main(void)
  {
 	puts("\"Programming is like building a multilingual puzzle");
