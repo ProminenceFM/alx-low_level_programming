cc25fb3fe579b2a25e48c0123022c2b15fd6820b
Folarin <folarinaluko123@yahoo.com>
2022-10-13 21:58:17 -0700

    ggc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main- Serves as the entry point
+ *
+ *Return: By default
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
