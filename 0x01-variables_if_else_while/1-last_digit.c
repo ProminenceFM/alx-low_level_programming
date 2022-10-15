ad0d74a97d074921d7288f8af85dfb6e662cf24c
Folarin <folarinaluko123@yahoo.com>
2022-10-14 21:11:39 -0700

    dff

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  *main-program will assign a random number to the variable n each time it is executed
  *
- *Return: always 0 for success 
+ *Return: always 0 for success
 */
 int main(void)
 {
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
  *main-program that prints the alphabet in lowercase, and then in uppercase
- *Return: always 0 for success 
- **/
+ *Return: always 0 for success
+**/
 int main(void)
 {
 	char i;
 	char j;
-
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
