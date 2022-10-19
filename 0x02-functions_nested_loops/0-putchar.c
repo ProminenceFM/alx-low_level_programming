f55367f60e7576708a0b36bce0fb3e9152fedef1
Folarin <folarinaluko123@yahoo.com>
2022-10-19 08:07:07 -0700

    fjfjf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - program that prints_putchar
  *
  * return: always 0 for success.
 */
-int _putchar()
+intmain(void)
 {
-	printf("_putchar\n");
-	return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+-putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\');
+_putchar('n');
 }
