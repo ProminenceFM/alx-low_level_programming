4cbf90f9674be9497a524931c06638457dfa114a
Folarin <folarinaluko123@yahoo.com>
2022-10-20 08:33:04 -0700

    kkdk

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - prints largest number
+ * @a: the first inputted number
+ * @b: second inputted number
+ * @c: third inputted number
+ * Return: largest number
+*/
+
+int largest_number (int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else 
+	{
+		largest = c;
+	}
+	return largest;
+}
---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_remaining_days - prints the remaining days of a year after a dt input
+ *@m: month integer
+ *@d: day integer
+ *@y: year integer
+ *Return: void
+*/
+
+void print_remaining_days(int m, int d, int y)
+{
+	if ((y % 400 == 0) || (y % 4 == 0 && 100 != 0))
+	{
+		if (month >= 3 && day >= 60)
+		{
+			d++;
+		}
+
+		printf("day = %d\n", d);
+		printf("Remaining days = %d\n", 366 - d);
+	}
+	else
+	{
+		if (m == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", m, d - 31, y);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", d);
+			printf("Remaining days: %d\n", 365 - d);
+		}
+	}
+}
