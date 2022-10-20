f5b06b7afff272d92e5ead00856a3c97241c4753
Folarin <folarinaluko123@yahoo.com>
2022-10-19 18:26:54 -0700

    ghsh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,10 @@ void print_alphabet_x10(void)
 {
 	char alph;
 	int count = 0;
-	while (count++ <=9)
+
+	while (count++ <= 9)
 	{
-		for(alph = 'a': alph <= 'z': alph++)
+		for (alph = 'a'; alph <= 'z'; alph++)
 			_putchar(alph);
 		_putchar('/n');
 	}
