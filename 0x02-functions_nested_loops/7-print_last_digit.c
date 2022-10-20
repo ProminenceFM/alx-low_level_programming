08cd393f092d70caf107864614a8a84d18b005ae
Folarin <folarinaluko123@yahoo.com>
2022-10-20 02:22:15 -0700

    jffjfjf2

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: number whose large digit should be printed
+ * by folarin
+ * Return:0 for success
+*/
+
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+	_putchar(r);
+}
