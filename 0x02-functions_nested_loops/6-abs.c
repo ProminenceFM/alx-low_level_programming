578fd12d00f77168dfdee990100cf7ce3f29d358
Folarin <folarinaluko123@yahoo.com>
2022-10-20 02:04:42 -0700

    jfjdfj

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _abs - print absolute value of an integer
+ * @n: the value to be checked
+ * by folarin
+ * Return: 0 always
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
