1d19650a09a6de83695439ef3a0e9696428ac685
Folarin <folarinaluko123@yahoo.com>
2022-10-19 19:52:30 -0700

    dfgf

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabets
+ * @c: variable to chevk for
+ * Return: 1 if alpha and 0 if othetwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c >= 'A' && c <= 'z' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
