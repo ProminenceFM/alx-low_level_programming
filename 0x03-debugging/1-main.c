7a7dd5cace6bb821247e3bda7b7558d1a9e6f15b
Folarin <folarinaluko123@yahoo.com>
2022-10-20 07:33:31 -0700

    djdjdjd

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - leadstoinfinite loop
+ * Return: 0
+ * by martins
+*/
+
+int main(void)
+{
+	printf("commencing infinite loop......\n")
+	
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("infiniteloop escaped \\o/\n");
+
+	return (0);
+}
