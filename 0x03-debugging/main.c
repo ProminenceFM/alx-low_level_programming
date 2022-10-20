3fe0c325dd259b0eaa67127386ad23998716a6dd
Folarin <folarinaluko123@yahoo.com>
2022-10-20 07:06:23 -0700

    debugging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -leads to an infinite loop
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop..... \n";
+	
+	i = 0;
+	
+	printf("infinite loop escaped \\o/\n");
+
+	return (0);
+}
