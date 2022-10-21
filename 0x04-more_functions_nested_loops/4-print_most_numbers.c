cfbf2082fa6838408338f719ae68d50a2456da02
Folarin <folarinaluko123@yahoo.com>
2022-10-21 06:12:10 -0700

    kskkskk

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,19 @@
 
 /*
  * print_most_numbers - mprintt  number 0-9 except 2 nd 4
- * Return: 0 to 9 except 2 to 4
+ * Return: 0
  */
 
 void print_most_numbers(void)
 {
 	int i;
-
-	for (i = 0; i <= 9; i++)
+	
+	for (i = 0; i < 9; i++)
 	{
 		if ((i != 2) && (i != 4))
 		{
 			_putchar(i)
 		}
 	}
+	return (0);
 }
