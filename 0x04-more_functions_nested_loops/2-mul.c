0e0b3dc56fc397ed4153c636510abf7ca5194bc2
Folarin <folarinaluko123@yahoo.com>
2022-10-20 21:52:55 -0700

    dd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+12 lines (11 sloc)  203 Bytes
+#include "main.h"
+
+/**
+ * mul - a function that multiplies integer
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the two inputs
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
