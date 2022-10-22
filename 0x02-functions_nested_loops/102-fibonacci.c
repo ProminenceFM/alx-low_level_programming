4e6de10c3f1e204ad6e5f04443b8ed1bd8a29af2
Folarin <folarinaluko123@yahoo.com>
2022-10-22 04:42:26 -0700

    ghj

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers.
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int count;
+	unsigned long a = 0, b = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = a + b;
+		printf("%lu", sum);
+
+		a = b;
+		b = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
