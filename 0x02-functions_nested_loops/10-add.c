22952a285568eda83967bb72303f8a81f30cea70
Folarin <folarinaluko123@yahoo.com>
2022-10-22 04:09:53 -0700

    ghhf

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - prints addition of 2 numbers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The result of the addition.
+ */
+int add(int a,  int b)
+{
+	return (a + b);
+}
